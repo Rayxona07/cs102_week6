2fe218510110ea78a8eb5d229c18ad35060ee4ca
Rayxona07 <250107@newuu.uz>
2025-10-02 21:11:45 +0500

    fewfqf

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 //
 // float product(float x,float y) {
@@ -12,19 +12,77 @@ using namespace std;
 //     cout<<"\nrounded:"<<product((int)x,(int)y);
 // }
 //problem 2
-double acceleration(double v1, double v2, double t) {
-   return (v2-v1)/t;
-}
-int main () {
-    double v1, v2, t;
-    cout<<"Enter the final velocity (v2): ";
-    cin>>v1;
-    cout<<"Enter the initial velocity(v1):";
-    cin>>v2;
-    cout<<"Enter the time (t): ";
-    cin>>t;
-    cout<<"Acceleration is: "<<acceleration(v1,v2,t);
-}
-    //problem3
-
-
+// double acceleration(double v1, double v2, double t) {
+//    return (v2-v1)/t;
+// }
+// int main () {
+//     double v1, v2, t;
+//     cout<<"Enter the final velocity (v2): ";
+//     cin>>v1;
+//     cout<<"Enter the initial velocity(v1):";
+//     cin>>v2;
+//     cout<<"Enter the time (t): ";
+//     cin>>t;
+//     cout<<"Acceleration is: "<<acceleration(v1,v2,t);
+// }
+    //problem 3
+// void fun(double r) {
+//     double M_PI=3.14;
+//     double circumference= 2*M_PI*r;
+//     double area= M_PI*r*r;
+//     cout<<"Circle circumference: "<<circumference<<endl;
+//     cout<<"Circle area: "<<area<<endl;
+// }
+// int main () {
+//     double radius;
+//     cout<<"Enter the radius of the circle: ";
+//     cin>>radius;
+//     fun(radius);
+// }
+//Problem 4
+// void fun(double r) {
+//     double circimference = 2*M_PI*r;
+//     double area= M_PI*r*r;
+//     cout<<"Circle circumference: "<<circimference<<endl;
+//     cout<<"Area: "<<area<<endl;
+// }
+// void fun(double a, double b) {
+//     double perimeter=2*(a*b);
+//     cout<<"Rectangle perimeter: "<<perimeter<<endl;
+//     cout<<"Rectangle area: "<<perimeter*perimeter<<endl;
+// }
+// int main() {
+//     double r,a,b;
+//     cout<<"Enter the radius of the circle: ";
+//     cin>>r;
+//     fun(r);
+//     cout<<"\nEnter sides of rectangle (a,b):";
+//     cin>>a>>b;
+//     fun(a,b);
+// }
+//Problem 5
+// void swapNumbers(int &x, int &y) {
+//     x=x+y;
+//     y=x-x;
+//     x=x-y;
+// }
+// int main() {
+//     int a, b;
+//     cout<<"Enter two integers: ";
+//     cin>>a>>b;
+//     cout<<"Before swap:a="<<a<<",b="<<b<<endl;
+//     swapNumbers(a, b);
+//     cout<<"After swap:a="<<a<<",b="<<b<<endl;
+// }
+//Problem 6
+// long long Factorial(int n=1) {
+//     if (n<0) {
+//         return 0;
+//     }
+//     long long result = 1;
+//     for (int i = 1; i <= n; ++i) {
+//         result *= i;
+//     }
+//     return result;
+// }
+//problem 7
